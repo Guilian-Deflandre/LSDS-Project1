1b11e828fd4e4775cfa521fbb080583cc0ddfcf2
10kbis <mattimmer@gmail.com>
2020-12-11 12:29:36 +0100

    Implements Raft election

---
+++ starter-code/repeat_timer.py
@@ -0,0 +1,35 @@
+# Shamelessly stolen from eraoul's anwser
+# https://stackoverflow.com/questions/474528/what-is-the-best-way-to-repeatedly-execute-a-function-every-x-seconds
+
+from threading import Timer
+from time import time
+
+class RepeatedTimer:
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.next_call = time()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self.next_call += self.interval
+            self._timer = Timer(self.next_call - time(), self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+    def restart(self):
+        self.stop()
+        self.start()
