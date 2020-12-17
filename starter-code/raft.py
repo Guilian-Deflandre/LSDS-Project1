d28cf126d897230fad2cebf8ae282d1ab9e6f52a
Guilian Deflandre <guilian200497@gmail.com>
2020-12-17 23:46:42 +0100

    Create some specifications

--- starter-code/raft.py
+++ starter-code/raft.py
@@ -57,8 +57,11 @@ class Node:
         self._time_last_requested_vote = now()
         self.state = NodeState.Follower
         self.peers = set()
-        self._election_timer = RepeatedTimer(election_timeout, self._election_timer_timeout)
-        self._leader_send_heartbeat_timer = RepeatedTimer(heartbeat, self._leader_send_heartbeat)
+        self._election_timer = RepeatedTimer(election_timeout,
+                                             self._election_timer_timeout)
+        self._leader_send_heartbeat_timer = RepeatedTimer(
+                                                heartbeat,
+                                                self._leader_send_heartbeat)
         self.term = 0
         self._voted_for = None
         self.leader = -1
@@ -85,7 +88,8 @@ class Node:
         RETURN
             /
         """
-        print(f'[Node {self.id}][{self.state.name}] Starting with peers {self.peers}')
+        print(f'[Node {self.id}][{self.state.name}] Starting with peers ' +
+              f'{self.peers}')
         app = FastAPI()
 
         app.get('/vote_request')(self.get_vote_request)
@@ -94,9 +98,9 @@ class Node:
         app.get('/request_action')(self.request_action)
         app.get('/stop')(self.stop)
         self._start_election_timer()
-        self.proc = Process(target=uvicorn.run, args=(app,),
-                            kwargs={"host":"127.0.0.1",
-                                    "port":"{}".format(5000 + self.id),
+        self.proc = Process(target=uvicorn.run, args=(app, ),
+                            kwargs={"host": "127.0.0.1",
+                                    "port": "{}".format(5000 + self.id),
                                     "log_level": "error"}, daemon=True)
         self.proc.start()
 
@@ -138,7 +142,7 @@ class Node:
         self._leader_send_heartbeat_timer.stop()
         self.state = NodeState.Dead
         self.proc.kill()
-
+        # os.kill(self.proc.pid, signal.SIGTERM)
 
     def _start_election_timer(self):
         """
@@ -176,7 +180,8 @@ class Node:
 
         def send_request(id):
             try:
-                request = VoteRequest(candidate=self.id, term=saved_current_term)
+                request = VoteRequest(candidate=self.id,
+                                      term=saved_current_term)
                 url = f'http://localhost:{5000 + id}/vote_request'
                 resp = requests.get(url, data=request.json())
                 if resp.status_code == 200:
@@ -189,8 +194,6 @@ class Node:
 
         vote_received = 0
         with ThreadPoolExecutor() as executor:
-            # if not main_thread().is_alive():
-            #     return
             replies = executor.map(send_request, self.peers)
             for reply in replies:
                 if reply is None:
@@ -201,44 +204,62 @@ class Node:
                 elif reply.term == saved_current_term and reply.granted:
                     vote_received += 1
 
-        # with self._lock:
-        if self.state == NodeState.Candidate and vote_received * 2 > len(self.peers):
+        if(self.state == NodeState.Candidate and
+           vote_received * 2 > len(self.peers)):
             # Won more than half of the votes, we are the leader now
             self.become_leader()
             return
 
-        # Did win the election nor found a node with a higher term, let's start a new election
+        # Did win the election nor found a node with a higher term, let's start
+        # a new election
         self._start_election_timer()
 
     def become_follower(self, term, leader):
         """
-        Initiate the election of a leader in the consensus cluster
+        Change the current state of self into a follower one
 
         PARAMETERS
-        /
+        term: int
+            The id of the current term
+        leader: int
+            The id of the current node in leader state
         RETURN
         /
         """
-        # print(f'[Node {self.id}][Follower] Now follower at term {term}')
-        # with self._lock:
         self.state = NodeState.Follower
         self.term = term
         self.leader = leader
         self._voted_for = None
         self._time_last_requested_vote = now()
 
-        # Start the election timer as a Follower switches to a Candidate if the timer timeout
+        # Start the election timer as a Follower switches to a Candidate if the
+        # timer timeout
         self._start_election_timer()
 
     def become_leader(self):
+        """
+        Change the current state of self into a leader one
+
+        PARAMETERS
+        /
+        RETURN
+        /
+        """
         print(f'[Node {self.id}][Leader] Now leader')
         self.state = NodeState.Leader
         self.leader = self.id
         self._leader_send_heartbeat_timer.start()
 
     def _leader_send_heartbeat(self):
-        # print(f'[Node {self.id}][Leader] Sending heartbeats')
-        # with self._lock:
+        """
+        Send the heartbeat as a leader to inform follower that it is still
+        alive
+
+        PARAMETERS
+        /
+        RETURN
+        /
+        """
         if self.state != NodeState.Leader:
             self._leader_send_heartbeat_timer.stop()
             return
@@ -247,7 +268,8 @@ class Node:
 
         def send_heartbeat(id):
             try:
-                request = AppendEntries(leader=self.id, term=saved_current_term)
+                request = AppendEntries(leader=self.id,
+                                        term=saved_current_term)
                 url = f'http://localhost:{5000 + id}/append_entries'
                 resp = requests.get(url, data=request.json())
                 if resp.status_code == 200:
@@ -259,26 +281,36 @@ class Node:
                 return None
 
         with ThreadPoolExecutor() as executor:
-            # if not main_thread().is_alive():
-            #     return
             replies = executor.map(send_heartbeat, self.peers)
             for i, reply in enumerate(replies):
                 if reply is None:
-                    print(f'[Node {self.id}][Leader] did not receive a heartbeat response')
-                    continue  # Node is down/network partition
+                    print(f'[Node {self.id}][Leader] did not receive a ' +
+                          f'heartbeat response')
+                    # Node is down/network partition
+                    continue
                 if reply.term > saved_current_term:
                     self.become_follower(reply.term, i)
                     return
 
     def get_vote_request(self, request: VoteRequest):
-        # with self._lock:
-        print(
-            f'[Node {self.id}][{self.state.name}] Received vote request from {request.candidate} at term {request.term}')
+        """
+        Get the vote of the election leader mechanism from all peers in the
+        consensus cluster
+
+        PARAMETERS
+        request: VoteRequest
+            A vote request made by a peer
+        RETURN
+        /
+        """
+        print(f'[Node {self.id}][{self.state.name}] Received vote request ' +
+              f'from {request.candidate} at term {request.term}')
         # TODO Check state=dead ?
         if request.term > self.term:
             self.become_follower(request.term, request.candidate)
 
-        if self.term == request.term and (self._voted_for is None or self._voted_for == request.candidate):
+        if self.term == request.term and (self._voted_for is None or
+           self._voted_for == request.candidate):
             reply = VoteReply(granted=True, term=self.term)
         else:
             reply = VoteReply(granted=False, term=self.term)
@@ -286,9 +318,16 @@ class Node:
         return reply
 
     def get_append_entries(self, entry: AppendEntries):
-        # with self._lock:
-        # print(f'[Node {self.id}][{self.state.name}] Received AppendEntries from {entry.leader} at term {entry.term}')
+        """
+        Get the append entries of the election leader mechanism from all peers
+        consensus cluster which are actions of computers
 
+        PARAMETERS
+        entry: AppendEntries
+            An append entry from a client
+        RETURN
+        /
+        """
         # TODO Check state=dead ?
         if entry.term > self.term:
             self.become_follower(entry.term, entry.leader)
@@ -304,7 +343,14 @@ class Node:
         return reply
 
     def _election_timer_timeout(self):
-        # with self._lock:
+        """
+        Trigger a timeout in the election mechanism and initiate a new one
+
+        PARAMETERS
+        /
+        RETURN
+        /
+        """
         if self.state == NodeState.Leader:
             self._election_timer.stop()
 
@@ -312,6 +358,9 @@ class Node:
             self._election_timer.stop()
 
         if now() - self._time_last_requested_vote >= self.election_timeout:
-            # Time elapsed since last VoteRequest received from the leader or from a candidate is more than timeout
+            '''
+            Time elapsed since last VoteRequest received from the leader or
+            from a candidate is more than timeout
+            '''
             self._election_timer.stop()
             self.start_election()
